4b7494dc696f6f8034caedd5d41a7c0f7e35ba78
Curt-H <Curt_H@126.com>
2018-07-20 14:41:20 +0800

    更新: 为app增加注释并修复了几个笔误

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-sfrom flask import Flask
+from flask import Flask
 from utils import log
+
+# 路由函数包导入
 from routes.routes_public import public_route as public
 from routes.routes_todo import todo_route as todo
 from routes.routes_weibo import weibo_route as weibo
@@ -8,25 +10,32 @@ from routes.routes_user import user_route
 
 
 def configured_app():
+    """
+    app配置函数, 用于配置Flask主程序的路由蓝图, 数据库等
+    :return: app                                      N
+    """
     server = Flask(__name__)
+
+    # 注册路由蓝图
     server.register_blueprint(public)
     server.register_blueprint(todo)
     server.register_blueprint(weibo)
     server.register_blueprint(api_weibo)
     server.register_blueprint(user_route)
-    log('URL MAP\n'.format(server.url_map))
+
     return server
 
 
 if __name__ == '__main__':
     app = configured_app()
+
+    # Flask运行参数配置, 正式执行时必须要关闭debug模式
     # debug 模式可以自动加载你对代码的变动, 所以不用重启程序
     # host 参数指定为 '0.0.0.0' 可以让别的机器访问你的代码
-    # app = Flask(__name__)
-    # register_route(app)
     config = dict(
         debug=True,
         host='0.0.0.0',
-        port=80,
+        port=8080,
     )
+
     app.run(**config)
