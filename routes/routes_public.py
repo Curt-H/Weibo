36e5b3d495f09649e3a3a2bdc484febef39a8303
Curt-H <Curt_H@126.com>
2018-07-20 16:08:36 +0800

    更新: 为pubulic路由增加注释

--- routes/routes_public.py
+++ routes/routes_public.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
-
 from routes import current_user
 
+# 注册Flask蓝图
 public_route = Blueprint('public_route', __name__)
 
 
@@ -10,6 +10,6 @@ def index():
     """
     主页的处理函数, 返回主页的响应
     """
-
     u = current_user()
+
     return render_template('index.html', username=u.username)
