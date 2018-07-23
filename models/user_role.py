289359ee3b23d00d7869f0f703e98b38fc1cf6a2
Curt-H <Curt_H@126.com>
2018-07-23 16:22:34 +0800

    移除: 移除有关人力jinja2的相关方法

--- models/user_role.py
+++ models/user_role.py
@@ -1,4 +1,3 @@
-import json
 from enum import (
     Enum,
     auto,
@@ -11,21 +10,3 @@ class UserRole(Enum):
 
     def translate(self, _escape_table):
         return self.name
-
-
-class GuaEncoder(json.JSONEncoder):
-    prefix = "__enum__"
-
-    def default(self, o):
-        if isinstance(o, UserRole):
-            return {self.prefix: o.name}
-        else:
-            return super().default(o)
-
-
-def gua_decode(d):
-    if GuaEncoder.prefix in d:
-        name = d[GuaEncoder.prefix]
-        return UserRole[name]
-    else:
-        return d
