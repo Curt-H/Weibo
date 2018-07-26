c4768b273f26e770abf61b3894388594ec191bf8
Curt-H <Curt_H@126.com>
2018-07-26 14:14:49 +0800

    修复: 移除Comment中的微博作者id, 避免循环引用

--- models/comment.py
+++ models/comment.py
@@ -2,9 +2,7 @@ import time
 
 from models.base_model import SQLModel
 
-# from models.weibo import Weibo
 from models.user import User
-from models.weibo import Weibo
 
 
 class Comment(SQLModel):
@@ -18,12 +16,8 @@ class Comment(SQLModel):
         self.user_id = form.get('user_id', user_id)
         self.weibo_id = int(form.get('weibo_id', -1))
         self.writer = self.get_writer()
-        self.weibo_writer = self.get_weibo_writer()
         self.update_time = form.get('update_time', int(time.time()))
         self.create_time = form.get('create_time', int(time.time()))
 
     def get_writer(self):
         return User.one(id=self.user_id).username
-
-    def get_weibo_writer(self):
-        return Weibo.one(id=self.weibo_id).user_id
